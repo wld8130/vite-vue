616c812882d8862f766c56999e8541f2e556d329
吴立东 <wulidong@dqalpha.org>
2023-08-24 15:12:47 +0800

    feat:新增stylint校验

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,14 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+  user-select: none;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
-
